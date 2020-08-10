e819e526325909c4e76c39066372dc161176b918
Dusch4593 <brandondusch@gmail.com>
2020-08-10 10:25:52 -0400

    created some more components

---
+++ src/components/Book.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Book = props => {
+  return (
+    <div>
+      Here is a Book!
+    </div>
+  )
+}
+
+export default Book
--- src/containers/ResultsContainer.js
+++ src/containers/ResultsContainer.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Book from '../components/Book'
 
 class ResultsContainer extends Component {
   render() {
     return (
       <div>
         The Results Container
+        < Book />
       </div>
     )
   }
